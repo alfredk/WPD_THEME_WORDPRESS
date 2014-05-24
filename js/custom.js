e2b15bf8586819edbe380fbb18da5150d9949542
AlfredK <alfred@aglobalway.com>
2014-05-24 12:58:14 -0700

    js edit

--- js/custom.js
+++ js/custom.js
@@ -4,12 +4,4 @@
   	$( "header .search-form" ).toggle('fast');
 	});
 
-	// $( ".search-field" ).toggle( showOrHide );
-
-	// if ( showOrHide === true ) {
-	//   $( ".search-field" ).show();
-	// } else if ( showOrHide === false ) {
-	//   $( ".search-field" ).hide();
-	// }
-
 })(jQuery); // Fully reference jQuery after this point.
