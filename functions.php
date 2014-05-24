3d12ab01fc6725240abc1ea686ace28d64a9bffe
AlfredK <alfred@aglobalway.com>
2014-05-24 13:13:42 -0700

    test edit

--- functions.php
+++ functions.php
@@ -94,6 +94,7 @@ function beny_scripts() {
 
 	wp_enqueue_script( 'beny-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
+	//add jQuery to array()
 	wp_enqueue_script( 'beny-custom', get_template_directory_uri() . '/js/custom.js', array('jquery'), '20140424', true );
 
 	// Fonts
